a34b9dbd64ac363cbdb80a97eef086b8b7e3f412
Anton <anton040795@gmail.com>
2022-02-26 21:01:23 +0400

    Выполнил домашку

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,5 +3,11 @@ package ru.skypro;
 
 public class Main {
     public static void main(String[] args) {
+        Book firstBook = new Book("Ono", new Author("Stiven","King"),1986);
+        Book secondBook = new Book("Flat World", new Author("Terry","Pratchett"),1983);
+        System.out.println("firstBook = " + firstBook);
+        System.out.println(secondBook);
+        firstBook.setYear(2020);
+        System.out.println(firstBook.setYear());
     }
 }
